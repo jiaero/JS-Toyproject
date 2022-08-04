dea64ad910973efadbc5ed3f6f2e1ca56d81d63a
jiaero <malns7@naver.com>
2022-08-05 02:11:53 +0900

    create alert, complete list function

--- main.js
+++ main.js
@@ -1,53 +1,46 @@
-const todoInput = document.getElementById("searchValue");
-const todoBtn = document.getElementById("add_btn");
+const todoInput = document.getElementById("searchValue");  // input 입력값
+const todoBtn = document.getElementById("add_btn");  // 추가 버튼
 
-function todoBtnClick(){
-    // 공백 상태에서 button을 누를 때 list가 추가되는 것을 방지
-    if(todoInput.value.length > 0) {
+function todoBtnClick() {
+
+    if(!todoInput.value) {
+        alert("Please enter your To Do List ! :-)");  // input에 내용을 입력하지 않고 추가 버튼 누르면 alert 발생
+    
+    } else {
+
         const ul = document.querySelector("ul");
-        const li = document.createElement("li");
-        const DeleteBtn = document.createElement("button");
+        const li = document.createElement("li");  // html에 'li' 태그 만들기
+        const div = document.createElement("div");  // html에 'div' 태그 만들기
+        const DeleteBtn = document.createElement("button");  // 삭제 버튼 생성
+
         ul.appendChild(li);
-        li.innerText = `${todoInput.value}`
-        li.setAttribute("class", "li");
-        li.appendChild(DeleteBtn);
-        DeleteBtn.innerText = "x";
-        DeleteBtn.setAttribute("class", "delete_btn");
+        li.appendChild(div);  // li에 'div' 자식으로 붙이기 
+        div.innerText = `${todoInput.value}`  // <li>입력된 할 일</li>
+        li.appendChild(DeleteBtn);  // li에 삭제 버튼을 자식으로 붙이기
+        todoInput.value = "";  // 할 일 입력창 초기화
+        
+        // class 추가
+        li.setAttribute("class", "li");  // li 태그에 'li'라는 클래스를 부여함.
+        DeleteBtn.setAttribute("class", "delete_btn");  // 삭제 버튼에 delete_btn이라는 클래스를 부여함.
+        
+        DeleteBtn.innerText = "x";  // 삭제 버튼 안에 "x" 추가
+        
+        function lineLi() {
+        div.style.textDecoration = div.style.textDecoration == "none" ? "line-through" : "none";
+        }
+        // 완료 된 리스트에 줄 긋기
+
+
         function deleteTodo() {
             const removeli = event.target.parentElement;
-            removeli.remove();
+            removeli.remove();                            // 클릭된 삭제 버튼의 부모 요소(li)를 삭제한다.
         }
 
+        li.addEventListener("click", lineLi);
         DeleteBtn.addEventListener("click", deleteTodo);
-
     }
 }
 
+
 todoBtn.addEventListener("click", todoBtnClick);
 
-
-
-
-
-
-// ul onclick Test
-// const ul = document.querySelector("ul");
-// ul.onclick = function(e){
-//     console.log(e.target);
-// }
-
-// ul, li, button 초기 설정
-// if(todoInput.value.length > 0) {
-//     const ul = document.querySelector("ul");
-//     const li = document.createElement("li");
-//     const deleteBtn = document.createElement("button")
-//     ul.appendChild(li);
-//     ul.appendChild(deleteBtn);
-// }
-
-// 쥬야호의 creatTextNode 예시
-// const bodyEl = document.querySelector('body'); 
-// const divEl = document.createElement('div'); 
-// const text = document.createTextNode('hello'); 
-// divEl.appendChild(text);
-// bodyEl.appendChild(divEl);
